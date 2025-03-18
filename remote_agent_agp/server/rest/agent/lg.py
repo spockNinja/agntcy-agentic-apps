6e1ea429fb701d71136a14dd35d9a1efeb3a4c8e
brisacoder <dev@brisa.ai>
2025-03-17 22:28:02 -0700

    Update lg.py

--- remote_agent_agp/server/rest/agent/lg.py
+++ remote_agent_agp/server/rest/agent/lg.py
@@ -1,4 +1,7 @@
-# Build the state graph
+# SPDX-FileCopyrightText: Copyright (c) 2025 Cisco and/or its affiliates.
+# SPDX-License-Identifier: Apache-2.0
+
+# Build the Langgraph Application
 
 import os
 import sys
