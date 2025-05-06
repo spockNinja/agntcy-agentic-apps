6fd4690b5ffabd657559f7f93fd4dfaa7a83c5de
Reginaldo Costa <regcosta@cisco.com>
2025-05-06 15:21:22 +0200

    chore: remove interrupt
    Signed-off-by: Reginaldo Costa <regcosta@cisco.com>

--- marketing-campaign/src/marketing_campaign/main_langgraph.py
+++ marketing-campaign/src/marketing_campaign/main_langgraph.py
@@ -7,43 +7,28 @@ from marketing_campaign.state import OverallState, ConfigModel
 from marketing_campaign import mailcomposer
 from marketing_campaign.email_reviewer import TargetAudience
 from langchain_core.runnables.config import RunnableConfig
-from langgraph.types import Command
 
 
 async def main():
     print("What marketing campaign do you want to create?")
     inputState = OverallState(
-        messages=[], operation_logs=[], has_composer_completed=False
+        messages=[],
+        operation_logs=[],
+        has_composer_completed=False
     )
     while True:
         usermsg = input("YOU [Type OK when you are happy with the email proposed] >>> ")
-
-        inputState.messages.append(
-            mailcomposer.Message(content=usermsg, type=mailcomposer.Type.human)
+        inputState.messages.append(mailcomposer.Message(content=usermsg, type=mailcomposer.Type.human))
+        output = await graph.ainvoke(inputState, RunnableConfig(
+            configurable=ConfigModel(
+                recipient_email_address=os.environ["RECIPIENT_EMAIL_ADDRESS"],
+                sender_email_address=os.environ["SENDER_EMAIL_ADDRESS"],
+                target_audience=TargetAudience.academic
+            ).model_dump()
         )
-
-        config = ConfigModel(
-            recipient_email_address=os.environ["RECIPIENT_EMAIL_ADDRESS"],
-            sender_email_address=os.environ["SENDER_EMAIL_ADDRESS"],
-            target_audience=TargetAudience.academic,
-            thread_id="2",
-        ).model_dump()
-        thread = RunnableConfig(configurable=config)
-
-        output = await graph.ainvoke(inputState, thread)
-
-        # Check if graph is interrupted by mailcomposer
-        if graph.get_state(thread).next != ():
-            commad = Command(
-                resume=mailcomposer.Message(
-                    content="I need it in html", type=mailcomposer.Type.human
-                ).model_dump()
-            )
-            # Send a signal to the graph to resume execution
-            await graph.ainvoke(commad, config=thread)
+)
 
         outputState = OverallState.model_validate(output)
-
         if len(outputState.operation_logs) > 0:
             print(outputState.operation_logs)
             break
